397b814ace782834b835c630b4302451edc2adaa
Supriti Vats <supritivats123@gmail.com>
2023-05-16 02:30:25 +0530

    x

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './mystyle.css';
 import Chart from 'chart.js/auto';
 import Papa from 'papaparse';
 
 function App() {
-  const [topWords, setTopWords] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [MostWords, setMostWords] = useState([]);
+  const [Fetching, setFetching] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
+
+  // code for fetching the chart from the provided link
+  const doSubmit = async (e) => {
+    e.preventDefault();
+    setFetching(true);
 
     const response = await fetch('https://www.terriblytinytales.com/test.txt');
-    const text = await response.text();
+    const sentence = await response.text();
 
-    const words = text.split(/\W+/);
-    const wordFreq = {};
+    const mywords = sentence.split(/\W+/);
+    const wordCount = {};
 
-    for (let word of words) {
-      if (word) {
-        word = word.toLowerCase();
-        wordFreq[word] = wordFreq[word] ? wordFreq[word] + 1 : 1;
+    for (let myword of mywords) {
+      if (myword) {
+        myword = myword.toLowerCase();
+        wordCount[myword] = wordCount[myword] ? wordCount[myword] + 1 : 1;
       }
     }
 
-    const topWords = Object.entries(wordFreq).sort((a, b) => b[1] - a[1]).slice(0, 20);
- 
-    setTopWords(topWords);
-    setLoading(false);
+    const MostOccured = Object.entries(wordCount).sort((a, b) => b[1] - a[1]).slice(0, 20);
+
+    setMostWords(MostOccured);
+    setFetching(false);
   };
 
-  const handleExport = () => {
-    const csv = Papa.unparse(topWords, { header: ['Word', 'Count'] });
+  //code for getting the most words in the chart in excel format
+
+  const doExport = () => {
+    const csv = Papa.unparse(MostWords, { header: ['Word', 'Count'] });
     const blob = new Blob([csv], { type: 'text/csv' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -40,56 +44,74 @@ function App() {
     a.click();
   };
 
-  React.useEffect(() => {
-    if (topWords.length) {
-      const labels = topWords.map(([word, count]) => word);
-      const data = topWords.map(([word, count]) => count);
+  // displaying the histogram depicting the most words in the chart with the frequency
 
-      const histogram = new Chart(document.getElementById('histogram'),
-        {
+  useEffect(() => {
+    const createHistogram = () => {
+      const data = MostWords.map(([word, total]) => total);
+      const labels = MostWords.map(([word, total]) => word);
+
+
+      const histogramElement = document.getElementById('chartHistogram');
+      if (histogramElement) {
+        const histogram = new Chart(histogramElement, {
           type: 'bar',
           data: {
             labels,
             datasets: [{
-              label:'Frequency of each words',
               data,
               borderWidth: 2,
+              label: 'Frequency of each word ',
               backgroundColor: 'rgba(23, 68, 128, 1)',
               borderColor: 'rgba(52, 131, 235, 1)',
-              
             }]
           },
           options: {
             responsive: true,
           }
-        }
-      );
+        });
 
-      return () => {
+        return histogram;
+      }
+
+      return null;
+    };
+
+    let histogram = createHistogram();
+
+    return () => {
+      if (histogram) {
         histogram.destroy();
-      };
-    }
-  }, [topWords]);
+        histogram = null;
+      }
+    };
+  }, [MostWords]);
+
+//===========================================================================================
 
   return (
     <><div className='introduction'>Terribly Tiny Tale Assignment By Supriti Vats</div>
-    <div className='mycontainer'>
-      
-      <form   onSubmit={handleSubmit}>
-        <button type="submit">{loading ? 'Loading...' : 'Submit'}</button>
-      </form>
+      <div className='mycontainer'>
 
-      {
-        topWords.length > 0 && (
-          <div style={{ width: '100%', height: '100%', }}>
-            <div style={{ width: '70%', height: '70%', marginLeft: '250px', marginTop: '30px' }}>
-              <canvas id="histogram"></canvas>
+        <form onSubmit={doSubmit}>
+
+          <button type="submit">{Fetching ? 'Fetching...' : 'Submit'}</button>
+        </form>
+
+        {
+          MostWords.length > 0 && (
+            <div style={{ width: '100%', height: '90%', }}>
+              <div style={{ width: '70%', height: '65%', marginLeft: '225px', marginTop: '20px' }}>
+                <canvas id="chartHistogram"></canvas>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', fontSize: '20px', fontWeight: 'bold', fontFamily: 'Times New Roman' }}>  X-axis =  Top 20 words with highest Frequency &nbsp; &nbsp;&nbsp;&nbsp;
+                Y-axis = Total Frequency
+              </div>
+              <button className='export-button' onClick={doExport}>Export</button>
             </div>
-            <button className='export-button' onClick={handleExport}>Export</button>
-          </div>
-        )
-      }
-    </div>
+          )
+        }
+      </div>
     </>
   );
 }
